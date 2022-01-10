284b9dd825346951b91b5d1c2eaf378918698de5
transistor <trans@jabberwocky.ca>
2022-01-09 21:32:53 -0800

    Fixed theme to load immediately

--- assets/theme.js
+++ assets/theme.js
@@ -1,25 +1,27 @@
 
-function setTheme(toggle, theme) {
+function setTheme(theme, toggle) {
     if (theme == 'light') {
         document.getElementById("pagetheme").setAttribute('href', "/assets/theme-light.css");
         document.getElementById("codetheme").setAttribute('href', "/assets/code-default.css");
-        toggle.innerText = 'dark';
+        if (toggle) toggle.innerText = 'dark';
     } else {
         document.getElementById("pagetheme").setAttribute('href', "/assets/theme-dark.css");
         document.getElementById("codetheme").setAttribute('href', "/assets/code-manni.css");
-        toggle.innerText = 'light';
+        if (toggle) toggle.innerText = 'light';
     }
 }
 
+var theme = window.localStorage.getItem('theme') || 'light';
+setTheme(theme);
+
 window.onload = function() {
-    var theme = window.localStorage.getItem('theme') || 'light';
     var toggle = document.getElementById("theme-toggle");
-    setTheme(toggle, theme);
+    setTheme(theme, toggle);
 
     toggle.onclick = function() {
         theme = (theme == 'light') ? 'dark' : 'light';
         window.localStorage.setItem('theme', theme);
-        setTheme(toggle, theme);
+        setTheme(theme, toggle);
     }
 }
 
